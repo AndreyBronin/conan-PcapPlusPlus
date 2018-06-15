e71aaba0bc1e71cf6ac9366d5eaba54d34f19f12
Andrey Bronin <jonnib@yandex.ru>
2018-06-15 18:07:16 +0300

    package_info link order

--- conanfile.py
+++ conanfile.py
@@ -11,12 +11,12 @@ class PcapplusplusConan(ConanFile):
     requires = "libpcap/1.8.1@uilianries/stable"
 
     def source(self):
-        git = tools.Git(folder="PcapPlusPlus")
-        git.clone("https://github.com/echo-Mike/PcapPlusPlus.git", "feature/build_system_rework")
-
+        #git = tools.Git(folder="PcapPlusPlus")
+        #git.clone("https://github.com/echo-Mike/PcapPlusPlus.git", "feature/build_system_rework")
+	self.run("git clone --depth 1 --branch feature/build_system_rework https://github.com/echo-Mike/PcapPlusPlus.git PcapPlusPlus")
 
     def build(self):
-        env_build = AutoToolsBuildEnvironment(self)
+        #env_build = AutoToolsBuildEnvironment(self)
 	libpcap_info = self.deps_cpp_info["libpcap"]
         include_path = libpcap_info.include_paths[0]
 	lib_path = libpcap_info.lib_paths[0]
@@ -26,12 +26,11 @@ class PcapplusplusConan(ConanFile):
             build_flags = '-I%s' % include_path
             build_flags += ' -L%s' % lib_path
 	    self.run("make -e PCAPPP_BUILD_FLAGS='%s' libs -j5" % build_flags)
-#            env_build.make(args=["make -e PCAPPP_BUILD_FLAGS='%s' libs -j5" % build_flags])
-
 
     def package(self):
         self.copy("*.h", dst="include", src="PcapPlusPlus/Dist/header")
         self.copy("*.a", dst="lib", keep_path=False)
 
     def package_info(self):
-        self.cpp_info.libs = tools.collect_libs(self)
+	self.cpp_info.libs = ["Packet++", "Pcap++", "Common++"]
+        #self.cpp_info.libs = tools.collect_libs(self)
