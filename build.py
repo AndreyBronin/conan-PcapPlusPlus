071bca1eefa7303f36850ac736c7c9599514a2b7
Andrey Bronin <jonnib@yandex.ru>
2018-06-18 14:27:37 +0300

    travis experiment

--- build.py
+++ build.py
@@ -2,6 +2,6 @@ from conan.packager import ConanMultiPackager
 
 
 if __name__ == "__main__":
-    builder = ConanMultiPackager(build_policy="missing")
+    builder = ConanMultiPackager()
     builder.add_common_builds()
     builder.run()
