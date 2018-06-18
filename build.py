ac0b29417b662a84e88c53298dd683f63ab2766a
Andrey Bronin <jonnib@yandex.ru>
2018-06-18 08:39:51 +0300

    Travis compile build_policy missing

--- build.py
+++ build.py
@@ -2,6 +2,6 @@ from conan.packager import ConanMultiPackager
 
 
 if __name__ == "__main__":
-    builder = ConanMultiPackager(args="--build missing")
+    builder = ConanMultiPackager(build_policy="missing")
     builder.add_common_builds()
     builder.run()
